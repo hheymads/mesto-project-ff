b1edc6b681c85741d75935d1b250f6ea1eff205b
Natalia Gazaryan <hheymads@yandex.ru>
2024-10-08 20:25:00 +0300

    добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция удаления карточки
+const handleDeleteCard = (cardsElement) => {
+    cardsElement.remove();
+}
+
+// @todo: Функция создания карточки
+const createCard = (elem, onDelete) => {
+    const cardsElement = cardTemplate.cloneNode(true);
+    const deleteBtn = cardsElement.querySelector('.card__delete-button');
+
+    // Установка данных для карточки
+    cardsElement.querySelector('.card__title').textContent = elem.name;
+    cardsElement.querySelector('.card__image').src = elem.link;
+
+    // Добавление обработчика события для кнопки удаления
+    deleteBtn.addEventListener('click', () => {
+    onDelete(cardsElement);
+    });
+
+    return cardsElement;
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (elem) {
+    const cardsElement = createCard(elem, handleDeleteCard);
+    placesList.append(cardsElement);
+});
